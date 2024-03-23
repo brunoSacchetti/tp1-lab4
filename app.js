4bee81ba3493d4edf9b04cdae002ab419891cedf
MaxiColombo <maximocolombo2003@gmail.com>
2024-03-23 18:08:10 -0300

    Ruteo vistas empresa, noticia

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express'); /* <- ruteo backend */
-const conexion = require('./database/database.js'); 
+const conexion = require('./database/database.js');
 
 const app = express();
 
-// Consulta de usuarios
+/* SISTEMA DE RUTEOS */
 app.get("/", (req, res) => {
-  res.json("funciona")
+  res.json("funciona") /* CREAR UN HOME */
 })
 
+/* ESTO ES PARA CARGAR VISTAS */
+app.get("/administrarEmpresa", (req, res) => {
+  res.sendFile(__dirname + "/adminEmpresa.html")
+})
 
+app.get("/administrarNoticia", (req, res) => {
+  res.sendFile(__dirname + "/administrarNoticia.html")
+})
 
-// Otros endpoints para alta, baja, modificación, etc.
-
-const puerto = 3300; // Puerto en el que se ejecutará el servidor
+const puerto = 3300;
 app.listen(puerto, () => {
   console.log(`Servidor en ejecución en http://localhost:${puerto}`);
 });
